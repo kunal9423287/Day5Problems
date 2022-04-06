66697510e4e5f053dd0a382cced4f3348752b7da
kunal9423287 <102022387+kunal9423287@users.noreply.github.com>
2022-04-06 17:09:02 +0530

    Added Harmonic number Series Problem

---
+++ HarmonicNumber.java
@@ -0,0 +1,23 @@
+package day5probs;
+import java.util.Scanner;
+public class HarmonicNumber {
+
+
+		public static void main(String[] args) {
+			System.out.print("Enter any number : ");
+			Scanner sc = new Scanner(System.in);
+			int num = sc.nextInt();
+			System.out.println("The Harmonic Number is : ");
+			double result = 0;
+			while(num > 0)
+			{
+				result = result + (double) 1 / num;
+				num--;
+				System.out.println(result +"  ");
+			}
+			System.out.println("");
+			System.out.println("Output of Harmonic Series is "+result);
+			
+	}
+
+}
